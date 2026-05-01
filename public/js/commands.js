fbbcedb2d0731e67c592d5da1ace3132f62f6192
Galberton <gustavo.borghezan030706@gmail.com>
2026-05-01 20:19:15 -0300

    Create commands.js

---
+++ public/js/commands.js
@@ -0,0 +1,52 @@
+export function handleCommand(input, { print, socket, setUsername }) {
+    const [commands, ...args] = input.split("");
+
+    switch (commands) {
+        case "/help":
+            print("Comandos disponiveis:", "system");
+            print("/help - listar comandos", "system");
+            print("/nick [nome] - definir nome", "system");
+            print("/clear - limpa terminal", "system");
+            print("/users - listar usuário online", "system");
+            print("/msg [usuario] [mensagem] - mensagem privada", "system");
+            break;
+
+            case "/clear":
+                document.getElementById("terminal").innerHTML = "";
+                break;
+
+            case "/nick":
+                const name = args[0];
+
+                if (!name) {
+                    print("Uso: /nick [nome]", "system")
+                    return;
+                }
+
+                setUsername(name);
+                socket.emit("set username", name)
+                print(`Nome definido como ${name}`, "system");
+                break;
+
+            case "/users":
+                socket.emit("get users");
+                break;
+
+            case "/msg":
+                const to = args[0]
+                const message = args.slice(1).join(" ");
+
+                if (!to || !message) {
+                    print("Uso: /msg [usuario] [message]", "system");
+                    return;
+                }
+
+                socket.emit("private message", {to, message});
+                print(`[PRIVADO -> ${to}] ${message}`, "private");
+                break;
+
+            default:
+                print("Comando não reconhecido. Digite /help", "system");
+
+    }
+}
